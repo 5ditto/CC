fb93e2634cb5d9919b5e02aeded5094058003b7f
Gonçalo Martins dos Santos <smgonsa@hotmail.com>
2022-11-06 17:28:33 +0000

    Atualizaç

--- TP2/cliente.py
+++ TP2/cliente.py
@@ -1,7 +1,9 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# AF_INET -> IPv4
+# SOCK_DGRAM -> UDP
 
 msg = "Adoro Redes :)"
 
-s.sendto(msg.encode('utf-8'), ('10.0.0.10', 3333))
+s.sendto(msg.encode('utf-8'), ('127.0.0.1', 12345))
