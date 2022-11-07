d1789eeecbc3b24d45c99ac246f6c0dc70ab2a24
Gonçalo Martins dos Santos <smgonsa@hotmail.com>
2022-11-07 16:08:22 +0000

    Merdas filhos da puta trabalhai

--- TP2/server.py
+++ TP2/server.py
@@ -1,11 +1,12 @@
+# Podemos considerar isto o SP
 import socket 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # AF_INET -> IPv4
 # SOCK_DGRAM -> UDP
 
-endereco = '127.0.0.1'
-porta = 12345
+endereco = '10.0.0.10'
+porta = 3333
 s.bind((endereco, porta ))
 
 print(f"Estou à escuta no {endereco}:{porta}") 
@@ -14,5 +15,7 @@ while True:
     msg, add = s.recvfrom(1024)
     print(msg.decode('utf-8'))
     print(f"Recebi uma mensagem do cliente {add}")
+
+#def geraRespQuery(msgQuery):  
 
 s.close()
