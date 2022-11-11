a3e12ccb6a0474c87120f663726a66d7c3bdb4c8
Gonçalo Martins dos Santos <smgonsa@hotmail.com>
2022-11-11 23:13:51 +0000

    Tá me a dar um erro estranho no sp quando faço bind à socket TCP

--- TP2/sp.py
+++ TP2/sp.py
@@ -4,109 +4,119 @@ import socket
 from dominio import Dominio
 import threading
 
-def geraRespQuery(msgQuery, dom):  
-    x = re.split(",|;", msgQuery)
-    msgId = x[0]
-    flags = x[1]
-    if "R" in flags:
-        flags = "R+A"
-    # Codificar aqui os casos de erro (1, 2 e 3)
-    responseCode = '0' # Não houve erros
-    dominio = x[6] 
-    typeValue = x[7]
-    nValues = str(len(dom.db[typeValue]))
-    nAuthorities = str(len(dom.db['NS']))  # Verificar isto
-    nExtraVal = str(len(dom.db['A']))      # Verificar isto
+class SP:
 
-    returnMsg = msgId + "," + flags + "," + responseCode + "," + nValues + "," + nAuthorities + "," + nExtraVal
-    returnMsg += ";" + dominio + "," + typeValue + ";\n"
+    def __init__(self):
+        self.dom = Dominio()
+        self.socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.dom.parseFicheiroConfig("config.txt")
+        self.dom.parseFicheiroBaseDados()
+        threading.Thread(target=self.conexaoTCP, args=()).start() # Thread que vai estar à escuta de novas ligações TCP
 
-    for elem in dom.db[typeValue]:
-        returnMsg += dominio + " " + typeValue + " " + elem + "\n"
-        returnMsg = returnMsg.replace("TTL", dom.db["TTL"]) # Substituir o "TTL" pelo seu respetivo valor 
 
-    for elem in dom.db['NS']:
-        returnMsg += dominio + " NS " + elem + "\n"
-        returnMsg = returnMsg.replace("TTL", dom.db["TTL"]) # Substituir o "TTL" pelo seu respetivo valor 
-    
-    #Parte dos servidores autoritários
-    for key in dom.db['A'].keys():
-        string = dom.db['A'][key].replace("TTL", dom.db["TTL"])
-        returnMsg += key + dominio + " " + "A" + string + "\n"
-    return returnMsg
+    def geraRespQuery(self, msgQuery, dom):  
+        x = re.split(",|;", msgQuery)
+        msgId = x[0]
+        flags = x[1]
+        if "R" in flags:
+            flags = "R+A"
+        # Codificar aqui os casos de erro (1, 2 e 3)
+        responseCode = '0' # Não houve erros
+        dominio = x[6] 
+        typeValue = x[7]
+        nValues = str(len(dom.db[typeValue]))
+        nAuthorities = str(len(dom.db['NS']))  # Verificar isto
+        nExtraVal = str(len(dom.db['A']))      # Verificar isto
 
-def transferenciaZona(connection, address, dom):
-    # Na transferência de zona o cliente é o SS e o servidor é o SP
-    # Primeiro recebe o nome completo do domínio
-    address = "193.123.5.189"
-    nomeDom = connection.recv(1024).decode('utf-8')
-    print("Nome: " + nomeDom)
-    nome = dom.name + "."
+        returnMsg = msgId + "," + flags + "," + responseCode + "," + nValues + "," + nAuthorities + "," + nExtraVal
+        returnMsg += ";" + dominio + "," + typeValue + ";\n"
 
-    if nomeDom != nome : # Nome de domínio inválido
-        print("Dominio inválido")
+        for elem in dom.db[typeValue]:
+            returnMsg += dominio + " " + typeValue + " " + elem + "\n"
+            returnMsg = returnMsg.replace("TTL", dom.db["TTL"]) # Substituir o "TTL" pelo seu respetivo valor 
+
+        for elem in dom.db['NS']:
+            returnMsg += dominio + " NS " + elem + "\n"
+            returnMsg = returnMsg.replace("TTL", dom.db["TTL"]) # Substituir o "TTL" pelo seu respetivo valor 
+
+        #Parte dos servidores autoritários
+        for key in dom.db['A'].keys():
+            string = dom.db['A'][key].replace("TTL", dom.db["TTL"])
+            returnMsg += key + dominio + " " + "A" + string + "\n"
+        return returnMsg
+
+    def transferenciaZona(self, connection, address):
+        # Na transferência de zona o cliente é o SS e o servidor é o SP
+        # Primeiro recebe o nome completo do domínio
+        address = "193.123.5.189" # Isto é só para puder testar (mais tarde vai sair)
+        nomeDom = connection.recv(1024).decode('utf-8')
+        print("Nome: " + nomeDom)
+        nome = self.dom.name + "."
+
+        if nomeDom != nome : # Nome de domínio inválido
+            print("Dominio inválido")
+            connection.close()
+            return False
+
+        autorizacao = False
+        for ip in self.dom.endSS:
+            # Quem está a pedir a transferência de zona tem permissão para receber uma cópia da base de dados
+            if ip == address:
+                autorizacao = True
+                break
+            
+        # Quem está a pedir a transferência de zona não tem permissão para receber uma cópia da base de dados
+        if autorizacao == False:
+            print("Não tem autorização")
+            connection.close()
+            return False
+
+        nrEntradas = self.dom.db['nrEntradas']
+        connection.sendall(str(nrEntradas).encode('utf-8'))
+        resposta = connection.recv(1024).decode('utf-8')
+        if resposta != str(nrEntradas):
+            print("Número de entradas não foi aceite")
+            connection.close()
+            return False
+
+        # Mandar cada linha da base de dados para o SS
+        f = open(self.dom.ficheiroDb, 'r')
+        i = 1
+        respostaDb = ''
+        for line in f:
+            respostaDb += str(i) + " " + line
+            i += 1
+        f.close()
+
+        connection.sendall(respostaDb.encode('utf-8'))
         connection.close()
-        return False
 
-    autorizacao = False
-    for ip in dom.endSS:
-        # Quem está a pedir a transferência de zona tem permissão para receber uma cópia da base de dados
-        if ip == address:
-            autorizacao = True
-            break
-        
-    # Quem está a pedir a transferência de zona não tem permissão para receber uma cópia da base de dados
-    if autorizacao == False:
-        print("Não tem autorização")
-        connection.close()
-        return False
+    def devolveVersaoDB(self, connection, address):
 
-    nrEntradas = dom.db['nrEntradas']
-    connection.sendall(str(nrEntradas).encode('utf-8'))
-    resposta = connection.recv(1024).decode('utf-8')
-    if resposta != str(nrEntradas):
-        print("Número de entradas não foi aceite")
-        connection.close()
-        return False
+        msg = connection.recv(1024).decode('utf-8')
+        if msg == 'VersaoDB':
+            connection.sendall(self.dom.db['SOASERIAL'].encode('utf-8'))
+            resposta = connection.recv(1024).decode('utf-8')
+            if resposta == 'continua':
+                self.transferenciaZona(connection, address)
+            elif resposta == 'termina': 
+                connection.close()
 
-    # Mandar cada linha da base de dados para o SS
-    f = open(dom.ficheiroDb, 'r')
-    i = 1
-    respostaDb = ''
-    for line in f:
-        respostaDb += str(i) + " " + line
-        i += 1
-    f.close()
+    # Função que espera por novas ligações TCP ao SP e depois chama a função transferênciaZona para as tratar
+    def conexaoTCP(self):
+        endereco = '127.0.0.1'
+        porta = 5555
+        self.socketTCP.bind((endereco, porta))
+        self.socketTCP.listen()
 
-    connection.sendall(respostaDb.encode('utf-8'))
-    connection.close()
+        while True:
+            connection, address = self.socketTCP.accept()
+            print(f"Recebi uma ligação do cliente {address}, conexão {connection}")
+            threading.Thread(target=self.devolveVersaoDB, args=(connection, address)).start()    
 
-# Função que espera por novas ligações TCP ao SP e depois chama a função transferênciaZona para as tratar
-def conexaoTCP(socketTCP, dom):
-    endereco = '127.0.0.1'
-    porta = 3333
-    socketTCP.bind((endereco, porta ))
-    socketTCP.listen()
-
-    while True:
-        connection, address = socketTCP.accept()
-        print(f"Recebi uma ligação do cliente {address}, conexão {connection}")
-        threading.Thread(target=transferenciaZona,args=(connection, address, dom)).start()    
-
- 
-sUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# AF_INET -> IPv4
-# SOCK_DGRAM -> UDP
-sTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# AF_INET -> IPv4
-# SOCK_DGRAM -> TCP
-
-d = Dominio()
-d.parseFicheiroConfig("config.txt")
-d.parseFicheiroBaseDados()
-
-# Uma thread executa a função conexão TCP e recebe o socket TCP como argumento
-threading.Thread(target=conexaoTCP, args=(sTCP, d)).start()
+sp = SP()
+sp.conexaoTCP()
 
 #endereco = '127.0.0.1'
 #porta = 12345
--- TP2/ss.py
+++ TP2/ss.py
@@ -1,46 +1,64 @@
 import socket 
 from dominio import Dominio
 
-# Primeira coisa que o SS faz é fazer a primeira transferência de zona
-# Na transferência de zona o cliente é o SS e o servidor é o SP
-# Falta implementar isto na transferência de zona:
-# O SS vai verificando se recebeu todas as entradas esperadas até um tempo predefinido se esgotar. Quando esse tempo terminar
-# o SS termina a conexão TCP e desiste da transferência de zona. Deve tentar outra vez após um intervalo de tempo igual a SOARETRY. 
-def transferenciaZona(dom):
-    dbString = ''
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect(('127.0.0.1', 3333))
+class SS:
 
+    def __init__(self):
+        self.dom = Dominio()
+        self.versaoDB = -1
+        self.db = dict()
+        self.dom.parseFicheiroConfig("config.txt")
+    
+    # Na transferência de zona o cliente é o SS e o servidor é o SP
+    # Falta implementar isto na transferência de zona:
+    # O SS vai verificando se recebeu todas as entradas esperadas até um tempo predefinido se esgotar. Quando esse tempo terminar
+    # o SS termina a conexão TCP e desiste da transferência de zona. Deve tentar outra vez após um intervalo de tempo igual a SOARETRY. 
+    def transferenciaZona(self, s):
+        dbString = ''
+        #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        #s.connect(('127.0.0.1', 3333))
         # Primeiro enviar o nome completo do domínio
-        msg = dom.name + "."
+        msg = self.dom.name + "."
         s.sendall(msg.encode('utf-8'))
-        print("Nome")
         nrEntradas = s.recv(1024) # recebe o número de entradas da db
         print(str(nrEntradas))
         s.sendall(nrEntradas)     # aceita respondendo com o mesmo número que recebeu
-
         while True:
             parteDb = s.recv(1024).decode('utf-8')
-            print("DB")
             if not parteDb:
-                print("Acabou")
+                # Faz o parse da string final da transferencia de zona para a "cache" do SS
+                self.dom.parseStringParaDB(dbString)
                 return dbString
             dbString += parteDb
+
+    # Primeira coisa que o SS faz é verificar se necessita fazer uma transferência de zona
+    # Como no inicio a base de dados do SS é vazia ele tem que fazer a transferència de zona
+    # Antes de fazer a transferência de zona o SS verifica se tem a sua db atualizada ou não
+    def verificaVersaoDB(self):
+        dbString = ''
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            s.connect(('127.0.0.1', 5555))
+
+            msg = 'VersaoDB'
+            s.sendall(msg.encode('utf-8'))
+
+            versao = s.recv(1024).decode('utf-8')
+            print(versao)
+            if versao != self.versaoDB:# Base de dados do SS está desatualizada
+                s.sendall('continua'.encode('utf-8'))
+                dbString = self.transferenciaZona(self.dom, s)
+            else:
+                s.sendall('termina'.encode('utf-8'))
+                s.close()
+        except Exception as ex: 
+            print("Exception Occurred: %s"%ex)
             s.close()
-    except Exception as ex:
-        print("Exception Occurred: %s"%ex)
-        s.close()
-    
-    return dbString
 
-# Antes de fazer a transferência de zona o SS verifica se tem a sua db atualizada ou não
-#def verificaVersaoDB(dom):
+        return dbString
 
-
-d = Dominio()
-d.parseFicheiroConfig("config.txt")
-string = transferenciaZona(d)
-print(string)
-d.parseStringParaDB(string)
-print(str(d.db))
+ss = SS()
+ss.verificaVersaoDB()
+print(str(ss.db))
